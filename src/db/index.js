fb85fbc1015e82308109d95caf5881876cb92192
Storm-py <ranamuneebmay2018@gmail.com>
2024-08-25 03:51:56 -0700

    I finished setting up the project for production

--- src/db/index.js
+++ src/db/index.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import {app} from '../app.js'
 import {DB_NAME} from '../constants.js'
 
 
@@ -6,6 +7,10 @@ const connectDB=async()=>{
     try{
         const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(`\n MongoDb connected !! DB HOST: ${connectionInstance.connection.host}`)
+        app.on('error',(error)=>{
+            console.log('ERRR :',error)
+            throw error;
+        })
     }catch(error) {
         console.log('MONGODB CONNECTION FAILED: ',error);
         process.exit(1)
